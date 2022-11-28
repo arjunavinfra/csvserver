1b22d9e75d011dd9133ceb6ae283fe34493e7485
Arjun <arjun@infracloud.io>
2022-11-28 12:36:29 +0530

    modified the script

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 Steps:
 
 1. Clone the repo https://github.com/arjunavinfra/csvserver.git
-2. Run the script 
+2. cd csvserver 
+3. Run the script ./gencsv (This script will generate a csv file)
+4. run the kubernetes manifest files by 
+   # kubectl apply -k ./ 
+5. Check the pod status 
+    #  kubectl get all -n csvserver
--- gencsv.sh
+++ gencsv.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 truncate -s 0 input.csv
-for i in `seq 10`
-do 
-    echo $i, $RANDOM >> input.csv
+INPUTFILE="input.csv"
+if [ $# == 0 ]; then 
+ echo "no arguments"
+ echo "generating 10 entries!!!"
+ N=10
+else 
+ echo "$# number of arguments"
+ N=$1
+fi
+
+for ((i = 0; i <= $N; i++)); do
+
+        echo "$i, ${RANDOM}">> $INPUTFILE
 done
+
+chmod o+r $INPUTFILE
